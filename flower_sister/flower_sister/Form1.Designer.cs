e8002bab5cba5ba757db3b65eef913c99973c871
oklinliang <jing_910119@163.com>
2018-07-31 18:10:24 +0800

    lianxi

---
+++ flower_sister/flower_sister/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace flower_sister
+{
+    partial class flower_sister
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(flower_sister));
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(59, 3);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(132, 21);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(59, 30);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(132, 21);
+            this.dateTimePicker2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "开始日";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "结束日";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(335, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "日志";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(197, 30);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(309, 272);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 279);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "计算";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(59, 84);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(97, 21);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "资金";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(162, 87);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 12);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "元";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(113, 114);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(17, 12);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "折";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(36, 114);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(17, 12);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "打";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(59, 111);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(48, 21);
+            this.textBox3.TabIndex = 10;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(24, 60);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(29, 12);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "利率";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(59, 57);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(48, 21);
+            this.textBox4.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(113, 60);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(11, 12);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "%";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(59, 138);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 21);
+            this.textBox5.TabIndex = 16;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(12, 141);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(41, 12);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "用户名";
+            // 
+            // flower_sister
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 314);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "flower_sister";
+            this.Text = "flower_sister-霾都亮亮";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ flower_sister/flower_sister/Form1.cs
@@ -0,0 +1,399 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text;
+
+namespace flower_sister
+{
+    public partial class flower_sister : Form
+    {
+        public flower_sister()
+        {
+            InitializeComponent();
+        }
+        public void output(string log)
+        {
+            textBox1.AppendText(log + "\r\n");
+        }
+        //输出利率
+        public void output_lilv(string log)
+        {
+            textBox4.AppendText(log + "\r\n");
+        }
+        public void output_zijin(string log)
+        {
+            textBox2.AppendText(log + "\r\n");
+        }
+        public void output_zhekou(string log)
+        {
+            textBox3.AppendText(log + "\r\n");
+        }
+        public void output_name(string log)
+        {
+            textBox5.AppendText(log + "\r\n");
+        }
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*天数计算方法------------------------------------------------------------------------------------------------------*/
+            DateTime start = Convert.ToDateTime(this.dateTimePicker1.Value.ToString("yyyy-MM-dd"));
+            DateTime end = Convert.ToDateTime(this.dateTimePicker2.Value.ToString("yyyy-MM-dd"));
+
+
+
+            string year1 = (start.Year.ToString());//起始年
+            int start_year = 0;
+            int.TryParse(year1, out start_year);
+
+            string year2 = (end.Year.ToString());//结束年
+            int end_year = 0;
+            int.TryParse(year2, out end_year);
+
+            string month1 = (start.Month.ToString());//起始月
+            int start_month = 0;
+            int.TryParse(month1, out start_month);
+
+            string month2 = (end.Month.ToString());//结束月
+            int end_month = 0;
+            int.TryParse(month2, out end_month);
+
+            string date1 = (start.Day.ToString());//起始日
+            int start_date = 0;
+            int.TryParse(date1, out start_date);
+
+            string date2 = (end.Day.ToString());//结束日
+            int end_date = 0;
+            int.TryParse(date2, out end_date);
+            if (start > end)
+            {
+                MessageBox.Show("开始时间应晚于结束时间");
+                return;
+            }
+
+            int start_month_date = 0;
+            start_month_date = System.DateTime.DaysInMonth(start_year, start_month);
+
+            int end_month_date = 0;
+            end_month_date = System.DateTime.DaysInMonth(end_year, end_month);
+            int list_end_month_date = 0;
+            if (end_month != 1)
+            {
+                list_end_month_date = System.DateTime.DaysInMonth(end_year, end_month - 1);
+            }
+            else
+            {
+                list_end_month_date = System.DateTime.DaysInMonth(end_year, 12);
+            }
+
+
+            int monthDiff = 0;
+            int dayDiff = 0;
+            int end_last_month = 0;
+            int differ_year = 0;
+            int differ_year_month = 0;
+
+            differ_year = end_year - start_year;
+            differ_year_month = differ_year * 12;
+
+
+            if (end_month - start_month == 1)
+            {
+                if (((end_year % 4 == 0 && end_year % 100 != 0) || (end_year % 100 == 0 && end_year % 400 == 0)) && ((start_date == 29 || start_date == 30 || start_date == 31) && end_date == 28))
+                {
+                    monthDiff = differ_year_month;
+                    dayDiff = 28;
+                }
+                else if (((end_year % 4 == 0 && end_year % 100 != 0) || (end_year % 100 == 0 && end_year % 400 == 0)) && ((end_date - start_date == 0) || (end_month == 2 && (start_date == 29 || start_date == 30 || start_date == 31) && end_date == 29)))
+                {
+                    monthDiff = 1 + differ_year_month;
+                    dayDiff = 0;
+                }
+                else if ((end_date - start_date == 0) || (end_month == 2 && (start_date == 28 || start_date == 29 || start_date == 30 || start_date == 31) && end_date == 28))
+                {
+                    monthDiff = 1 + differ_year_month;
+                    dayDiff = 0;
+                }
+                else if (start_month_date == 31)
+                {
+                    dayDiff = 31 - start_date;
+                    dayDiff = dayDiff + end_date;
+                    if (dayDiff > start_month_date)
+                    {
+                        dayDiff -= start_month_date;
+                        monthDiff += 1;
+                        monthDiff += differ_year_month;
+                    }
+                    if (dayDiff == end_month_date && start_date == 31)
+                    {
+                        dayDiff = 0;
+                        monthDiff += 1;
+                        monthDiff += differ_year_month;
+                    }
+                }
+                else if (start_month_date == 30)
+                {
+                    dayDiff = 30 - start_date;
+                    dayDiff = dayDiff + end_date;
+                    if (dayDiff > start_month_date)
+                    {
+                        dayDiff -= start_month_date;
+                        monthDiff += 1;
+                        monthDiff += differ_year_month;
+                    }
+                }
+                else if (start_month_date == 28)
+                {
+                    dayDiff = 28 - start_date;
+                    dayDiff = dayDiff + end_date;
+                    if (dayDiff > start_month_date)
+                    {
+                        dayDiff -= start_month_date;
+                        monthDiff += 1;
+                        monthDiff += differ_year_month;
+                    }
+                }
+                else if (start_month_date == 29)
+                {
+                    dayDiff = 29 - start_date;
+                    dayDiff = dayDiff + end_date;
+                    if (dayDiff > start_month_date)
+                    {
+                        dayDiff -= start_month_date;
+                        monthDiff += 1;
+                        monthDiff += differ_year_month;
+                    }
+                }
+
+            }
+            else if (end_month - start_month != 1)
+            {
+                end_last_month = end_month - 1;
+                if (end_last_month == 0)
+                {
+                    end_last_month = 12;
+                }
+                start_month_date = System.DateTime.DaysInMonth(start_year, end_last_month);
+                monthDiff = end_month - start_month - 1;
+                if (((end_year % 4 == 0 && end_year % 100 != 0) || (end_year % 100 == 0 && end_year % 400 == 0)) && ((start_date == 29 || start_date == 30 || start_date == 31) && end_date == 28))
+                {
+                    monthDiff += differ_year_month;
+                    dayDiff = 28;
+                }
+                else if (((end_year % 4 == 0 && end_year % 100 != 0) || (end_year % 100 == 0 && end_year % 400 == 0)) && ((end_date - start_date == 0) || (end_month == 2 && (start_date == 29 || start_date == 30 || start_date == 31) && end_date == 29)))
+                {
+                    monthDiff += 1;
+                    monthDiff += differ_year_month;
+                    dayDiff = 0;
+                }
+                else if ((end_date - start_date == 0) || (end_month == 2 && (start_date == 28 || start_date == 29 || start_date == 30 || start_date == 31) && end_date == 28))
+                {
+                    monthDiff += 1;
+                    monthDiff += differ_year_month;
+                    dayDiff = 0;
+                }
+                else if (start_month_date == 31)
+                {
+                    dayDiff = 31 - start_date;
+                    dayDiff = dayDiff + end_date;
+                    if (dayDiff > start_month_date)
+                    {
+                        dayDiff -= start_month_date;
+                        monthDiff += 1;
+                        monthDiff += differ_year_month;
+                    }
+                    else if (dayDiff == end_month_date && start_date == 31)
+                    {
+                        dayDiff = 0;
+                        monthDiff += 1;
+                        monthDiff += differ_year_month;
+                    }
+                    else
+                    {
+                        monthDiff += differ_year_month;
+                    }
+                }
+                else if (start_month_date == 30)
+                {
+                    dayDiff = 30 - start_date;
+                    if (dayDiff < 0)
+                    {
+                        dayDiff = 0;
+                    }
+                    dayDiff = dayDiff + end_date;
+                    if (dayDiff > start_month_date)
+                    {
+                        dayDiff -= start_month_date;
+                        monthDiff += 1;
+                        monthDiff += differ_year_month;
+                    }
+                    else
+                    {
+                        monthDiff += differ_year_month;
+                    }
+                }
+                else if (start_month_date == 28)
+                {
+                    dayDiff = 28 - start_date;
+                    if (dayDiff < 0)
+                    {
+                        dayDiff = 0;
+                    }
+                    dayDiff = dayDiff + end_date;
+                    if (dayDiff > start_month_date)
+                    {
+                        dayDiff -= start_month_date;
+                        monthDiff += 1;
+                        monthDiff += differ_year_month;
+                    }
+                    else
+                    {
+                        monthDiff += differ_year_month;
+                    }
+                }
+                else if (start_month_date == 29)
+                {
+                    dayDiff = 29 - start_date;
+                    if (dayDiff < 0)
+                    {
+                        dayDiff = 0;
+                    }
+                    dayDiff = dayDiff + end_date;
+                    if (dayDiff > start_month_date)
+                    {
+                        dayDiff -= start_month_date;
+                        monthDiff += 1;
+                        monthDiff += differ_year_month;
+                    }
+                    else
+                    {
+                        monthDiff += differ_year_month;
+                    }
+                }
+            }
+            dayDiff += 1;
+            if (dayDiff == end_month_date)
+            {
+                dayDiff = 0;
+                monthDiff += 1;
+            }
+
+            
+            string op_lilv = string.Empty;
+            string op_zijin = string.Empty;
+            string op_zhekou = string.Empty;
+            string op_name = string.Empty;
+            List<string> lines = new List<string>();
+            using (StreamReader reader_lilv = new StreamReader(@"G:\nlilv.txt"))
+            using (StreamReader reader_name = new StreamReader(@"G:\nname.txt"))
+            using (StreamReader reader_zhekou = new StreamReader(@"G:\nzhekou.txt"))
+            using (StreamReader reader_zijin = new StreamReader(@"G:\nzijin.txt"))
+            {
+                op_lilv = reader_lilv.ReadLine();
+                op_name = reader_name.ReadLine();
+                op_zhekou = reader_zhekou.ReadLine();
+                op_zijin = reader_zijin.ReadLine();
+                while (op_lilv != "" && op_lilv != null)
+                {
+                    output_lilv(op_lilv.ToString());
+                    output_zijin(op_zijin.ToString());
+                    output_zhekou(op_zhekou.ToString());
+                    output_name(op_name.ToString());
+                    if (MessageBox.Show("继续？", "caption", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+
+                        double dazhe = Convert.ToDouble(this.textBox3.Text.ToString());
+                        double new_dazhe = dazhe / 10;
+                        double my_money = Convert.ToDouble(this.textBox2.Text.ToString());
+                        double lilv = Convert.ToDouble(this.textBox4.Text.ToString());
+                        string name = textBox5.Text;
+                        double exit_rate = 0.02;
+                        double dazhe_out;
+                        double dazhe_qian;
+                        double cha;
+                        double gross_interest;
+                        double day_money;
+                        double overall_money;
+                        if (monthDiff == 0)
+                        {
+                            if (start_date < end_date)
+                            {
+                                day_money = my_money * lilv * dayDiff / 12 / end_month_date;
+                            }
+                            else
+                            {
+                                day_money = my_money * lilv * dayDiff / 12 / list_end_month_date;
+                            }
+                        }
+                        else
+                        {
+                            if (start_date < end_date)
+                            {
+                                day_money = my_money * lilv * dayDiff / 12 / end_month_date;
+                            }
+                            else
+                            {
+                                day_money = my_money * lilv * dayDiff / 12 / list_end_month_date;
+                            }
+                        }
+                        gross_interest = my_money * lilv / 12 * monthDiff;
+                        gross_interest += day_money;
+                        gross_interest = Math.Round(gross_interest, 5, MidpointRounding.AwayFromZero);
+
+                        overall_money = my_money + gross_interest;
+
+
+                        dazhe_out = my_money * exit_rate * new_dazhe;
+                        dazhe_qian = my_money * exit_rate;
+                        cha = dazhe_qian - dazhe_out;
+                        overall_money -= dazhe_out;
+
+
+                        output("用户名：" + name.ToString());
+                        output("共持有" + monthDiff.ToString() + "个月" + dayDiff.ToString() + "天");
+                        output("折扣：" + dazhe.ToString());
+                        output("利率" + lilv.ToString() + "%");
+                        output("总利息" + gross_interest.ToString() + "元");
+                        output("总成交额" + overall_money.ToString() + "元");
+                        output("打折前应收手续费：" + dazhe_qian.ToString());
+                        output("打折后应收手续费：" + dazhe_out.ToString());
+                        output("为您节省了：" + cha.ToString());
+                        output("-------------------------------------");
+                        textBox2.Text = "";
+                        textBox3.Text = "";
+                        textBox4.Text = "";
+                        textBox5.Text = "";
+
+                        lines.Add(op_lilv);
+                        Console.WriteLine(op_lilv);
+                        op_lilv = reader_lilv.ReadLine();
+
+                        lines.Add(op_zijin);
+                        Console.WriteLine(op_zijin);
+                        op_zijin = reader_zijin.ReadLine();
+
+                        lines.Add(op_zhekou);
+                        Console.WriteLine(op_zhekou);
+                        op_zhekou = reader_zhekou.ReadLine();
+
+                        lines.Add(op_name);
+                        Console.WriteLine(op_name);
+                        op_name = reader_name.ReadLine();
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
